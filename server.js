1489a8d2ecb9508f5a74c6570255db46967f7c09
Ian Jennings <ian@meetjennings.com>
2023-07-17 18:56:14 -0500

    update demo for dashcam

---
+++ server.js
@@ -0,0 +1,41 @@
+const PubNub = require("pubnub");
+
+const pubnub = new PubNub({
+  publishKey: "pub-c-9c98cd2a-92ee-4951-a92c-3796ee1b739d",
+  subscribeKey: "sub-c-06995b2a-b1f3-4079-929a-056e9d243775",
+  userId: "server",
+});
+
+// add listener
+const listener = {
+  status: (statusEvent) => {
+    if (statusEvent.category === "PNConnectedCategory") {
+      console.log("Connected");
+    }
+  },
+  message: (messageEvent) => {
+    showMessage(messageEvent.message);
+  },
+  presence: (presenceEvent) => {
+    // handle presence
+  },
+};
+pubnub.addListener(listener);
+
+// publish message
+const publishMessage = async (message) => {
+  await pubnub.publish({
+    channel: "chat-channel",
+    message: {
+      message: message,
+    },
+  });
+};
+
+// subscribe to a channel
+pubnub.subscribe({
+  channels: ["chat-channel"],
+});
+const showMessage = (msg) => {
+  console.log("message: " + msg);
+};
